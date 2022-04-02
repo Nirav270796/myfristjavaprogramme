91b33543d046705635f4bd4dd9e335752b495714
Krish Patel <72159332+gamestar2807@users.noreply.github.com>
2022-04-02 13:54:21 +0100

    Update MyFirstJavaProgram.java

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -5,6 +5,6 @@ import jdk.nashorn.internal.parser.JSONParser;
 public class MyFirstJavaProgram {
     public static void main(String[] args){
     System.out.println("Nirav Patel");
-        System.out.println("Hello World");
+        System.out.println("Krish has seen this file and he says Hello");
     }
 }
